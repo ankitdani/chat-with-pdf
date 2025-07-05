6abb871608dd629474e8aa694252f37c182a07c8
unknown <ankitdani1997@gmail.com>
2025-07-05 12:15:38 -0700

    Created file upoader component

---
+++ components/FileUploader.tsx
@@ -0,0 +1,46 @@
+"use client";
+
+import { CircleArrowDown, RocketIcon } from "lucide-react";
+import React, { useCallback } from "react";
+import { useDropzone } from "react-dropzone";
+
+const FileUploader = () => {
+  const onDrop = useCallback((acceptedFiles: File[]) => {
+    // Do something with the files
+    console.log(acceptedFiles);
+  }, []);
+  const { getRootProps, getInputProps, isDragActive, isFocused } = useDropzone({
+    onDrop,
+  });
+  return (
+    <div className="flex flex-col gap-4 items-center max-w-7xl mx-auto">
+      <div
+        {...getRootProps()}
+        className={`p-10 border-2 border-dashed mt-10 w-[90%] 
+          border-indigo-600 text-indigo-600 rounded-lg h-96 flex items-center text-center justify-center ${
+            isFocused || isDragActive ? "bg-indigo-300" : "bg-indigo-100"
+          }`}
+      >
+        <input {...getInputProps()} />
+        <div className="flex items-center flex-col justify-center">
+          {isDragActive ? (
+            <>
+              <RocketIcon className="h-20 w-20 animate-ping" />
+              <p>Drop the files here ...</p>
+            </>
+          ) : (
+            <>
+              <CircleArrowDown className="h-20 w-20 animate-bounce" />
+              <p>
+                Drag &apos;n&apos; drop some files here, or click to select
+                files
+              </p>
+            </>
+          )}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default FileUploader;
