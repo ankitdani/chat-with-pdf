fd2c69a77dead857bbc073e4385c525d0d1d856b
unknown <ankitdani1997@gmail.com>
2025-07-05 18:37:08 -0700

    Added rule in eslint config to ignore unused variables

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -11,6 +11,11 @@ const compat = new FlatCompat({
 
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
+  {
+    rules: {
+      "no-unused-vars": "off", // disables unused var errors globally
+    },
+  },
 ];
 
 export default eslintConfig;
